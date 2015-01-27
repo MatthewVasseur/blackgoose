1c4821abaa5822b4c8da53ef258c952889d0bbc2
Matthew Vasseur <mattvasseur@gmail.com>
2015-01-27 16:16:33 -0500

    devise :(

--- app/models/escort.rb
+++ app/models/escort.rb
@@ -1,9 +1,10 @@
 class Escort < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable,
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :timeoutable, :authentication_keys => [:loginKey]
+         :timeoutable, :authentication_keys => [:loginKey]
 
   # for generating random tokens (i.e. numbers)
   include Tokenable
