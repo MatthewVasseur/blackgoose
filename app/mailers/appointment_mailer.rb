54881a557f51edb97a3bc55ca481d200f8ae1878
Matthew Vasseur <mattvasseur@gmail.com>
2015-01-28 00:48:30 -0500

    mail nada

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,17 +1,3 @@
-require 'mail'
-
-Mail.defaults do
-  delivery_method :smtp, {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-end
-
 class AppointmentMailer < ApplicationMailer
   default from: "matt@marmar.is"
 
