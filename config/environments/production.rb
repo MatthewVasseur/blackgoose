81e9d7dab49ef6e450d8b12a2993ae2546e8ae06
Matthew Vasseur <mattvasseur@gmail.com>
2015-01-29 02:45:19 -0500

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # action mailer host
+  config.action_mailer.default_url_options = { :host => 'marmar-is.herokuapp.com' }
+
 end
